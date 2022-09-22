b955015e32e9624c59049d02779b98c160076b5e
Arthur Guelle <arthurguelle@gmail.com>
2022-09-21 23:45:01 -0300

    first commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+System.Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia("125855", "Modelo 1", "444444", "64");
+nokia.Ligar();
+nokia.InstalarAplicativo("Telegram");
+
+System.Console.WriteLine("---------------------------");
+
+System.Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone("125855", "Modelo 2", "44445844", "128");
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
